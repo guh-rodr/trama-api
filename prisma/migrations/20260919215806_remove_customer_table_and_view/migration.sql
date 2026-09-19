602ab868e801c377fb378d15c4ef559476733479
Gustavo Rodrigues <dev.gustavor@outlook.com>
2026-09-19 19:06:12 -0300

    refactor: drop customer table/view and their dependents

---
+++ prisma/migrations/20260919215806_remove_customer_table_and_view/migration.sql
@@ -0,0 +1,15 @@
+/*
+Warnings:
+
+- You are about to drop the `Customer` table. If the table is not empty, all the data it contains will be lost.
+
+ */
+-- DropView
+DROP VIEW IF EXISTS "CustomerStats";
+
+DROP VIEW IF EXISTS "ReceivableStats";
+
+DROP VIEW IF EXISTS "SaleStats";
+
+-- DropTable
+DROP TABLE "Customer";
